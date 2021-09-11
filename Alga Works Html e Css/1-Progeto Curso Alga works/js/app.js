7a115cf9c2517aded01aabf1e274a5e1c5422eb8
Eduardo-Santos-01 <eduardosantos123444@gmail.com>
2021-09-10 21:00:00 -0300

    add aulas

---
+++ Alga Works Html e Css/1-Progeto Curso Alga works/js/app.js
@@ -0,0 +1,11 @@
+alternador=document.querySelector('.js-botao__chaveador');
+
+alternador.onclick=function(){
+    menu=document.querySelector('.js-menu');
+
+    menu.classList.toggle('menu--exibindo')
+}
+
+    
+
+     
