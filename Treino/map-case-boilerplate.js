8527a57e90dd02ac80b4b7cca0dfb6cab65f8964
Eduardo-Santos-01 <eduardosantos123444@gmail.com>
2021-10-22 19:19:41 -0300

    treino js

---
+++ Treino/map-case-boilerplate.js
@@ -0,0 +1,16 @@
+const products = [
+  { name: 'Mouse Sem Fio', price: 30 },
+  { name: 'Pen Drive', price: 25 },
+  { name: 'Cartucho de Tinta', price: 50 },
+  { name: 'Suporte Ergonômico para Notebook', price: 23 },
+  { name: 'Repetidor de Sinal Wi-Fi', price: 44 }
+]
+
+const saleProduts=products.map(product=>{
+if(product.price>=30){
+
+return {name:product.name,price:product.price/2}
+}
+})
+console.log(products);
+console.log(saleProduts);
