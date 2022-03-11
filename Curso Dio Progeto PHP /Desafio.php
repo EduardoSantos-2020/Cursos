09e0b90c38e37d49764bde66c63939c5c89b11fa
EduardoSantos-2020 <eduardosantos123444@gmail.com>
2022-03-10 22:58:56 -0300

    add aulas

---
+++ Curso Dio Progeto PHP /Desafio.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+        <title>Desafio de codigo</title>
+    </head>
+    <body>
+        <div class="section--colus">
+            <h2>Titulo 1</h2>
+            <div>
+                <p>
+
+                    Mussum Ipsum, cacilds vidis litro abertis. Todo mundo vê os porris que eu tomo,
+                    mas ninguém vê os tombis que eu levo! Aenean aliquam molestie leo, vitae iaculis 
+                    nisl. Posuere libero varius. Nullam a  nisl ut ante blandit hendrerit. 
+                    Aenean sit amet nisi. Mé faiz elementum girarzis, nisi eros vermeio.
+                </p>
+            </div>
+            <h2>Titulo 2</h2>
+            <div>
+                <p>
+                    Mussum Ipsum, cacilds vidis litro abertis. Todo mundo vê os porris que eu tomo,
+                    mas ninguém vê os tombis que eu levo! Aenean aliquam molestie leo, vitae iaculis 
+                    nisl. Posuere libero varius. Nullam a  nisl ut ante blandit hendrerit. 
+                    Aenean sit amet nisi. Mé faiz elementum girarzis, nisi eros vermeio.
+                </p>
+            </div>
+            <h2>Titulo 3</h2>
+            <div>
+                <p>
+                    Mussum Ipsum, cacilds vidis litro abertis. Todo mundo vê os porris que eu tomo,
+                    mas ninguém vê os tombis que eu levo! Aenean aliquam molestie leo, vitae iaculis 
+                    nisl. Posuere libero varius. Nullam a  nisl ut ante blandit hendrerit. 
+                    Aenean sit amet nisi. Mé faiz elementum girarzis, nisi eros vermeio.
+                </p>
+            </div>
+        </div>
+    </body>
+    <script>
+        $(function(){
+            $(".section--colus").accordion()
+        })
+    </script>
+</html>
---
+++ Curso Dio Progeto PHP /index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Meu primeiro progama PHP</h1>
+        <?php
+
+        for( $i=0 ; $i < 10 ; $i++ )
+
+            print("Contagem = ".$i."<br />") 
+            
+        ?>
+    </body>
+</html>
+
+
