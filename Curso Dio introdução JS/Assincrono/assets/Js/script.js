cb82f2d16f5d3b4a91aaf1e57f3c1a6eb70fcf7e
EduardoSantos-2020 <eduardosantos123444@gmail.com>
2022-11-07 17:19:10 -0300

    aula assicrono

---
+++ Curso Dio introdução JS/Assincrono/assets/Js/script.js
@@ -0,0 +1,41 @@
+const Base_Url='https://dog.ceo/api/breeds/image/random';
+const btnDog=document.getElementById('change-dog');
+
+const imgDogs = async()=>{
+    const data=await fetch(Base_Url)
+    .then((res)=>res.json())
+    .catch((e)=>console.log(e))
+        
+    return data.message;
+    
+}
+
+
+// const imgDogs = async()=>{
+
+//     try{   
+
+//         const getDogs=await fetch(Base_Url);
+//         const json=await getDogs.json();
+        
+//     return json.message;
+    
+//     }catch(e){
+
+//     console.log(e.message);
+
+//     }
+// }
+
+
+
+const loadingImg=async()=>{
+const CampImg=document.getElementById('dog');
+CampImg.src= await imgDogs();
+}
+
+loadingImg()
+
+btnDog.addEventListener('click',loadingImg)
+
+
