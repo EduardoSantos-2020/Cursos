7fcf542f036e1600ff0bbdd5e19cbbabb4baccbc
EduardoSantos-2020 <eduardosantos123444@gmail.com>
2023-07-23 19:51:41 -0300

    ajustes complemetares

--- Curso Dio introdução JS/desafio To-do-List/assets/script.js
+++ Curso Dio introdução JS/desafio To-do-List/assets/script.js
@@ -1,21 +1,30 @@
 0
 let Banco = [{ 'tarefa': 'Estudar as 8 horas da manhã', 'Status': 'checked' },];
 
-const criandoItem = (Text, Status = " ", indice) => {
-
-let item = document.createElement('div')
+const criandoItem = (Text, Status = ' ', indice) => {
+    let item = document.createElement('div')
     item.classList.add('Itemcheck');
-    item.innerHTML= `<label class="container-check">
+    item.innerHTML = `<label class="container-check">
                         <input class="check" type=checkbox ${Status} data-indice=${indice}>
                         <div class="checkmark"></div>
                     </label>
                     <div class="container-text">
                         <p class="text-list" data-indice=${indice}>${Text}</p>
                     </div>
-                    <button type="button"  class="fa-solid fa-xmark" data-indice=${indice}></button>`;
+                    <button type="button" class="fa-solid fa-xmark" data-indice=${indice} ></button>`;
 
     document.getElementById('ItemList').appendChild(item);
-    TextStyle(indice);
+
+    itemBox = document.querySelectorAll('.check')[indice];
+
+    label = document.getElementsByClassName('text-list')[indice]
+
+    if (itemBox.checked) {
+        label.style.textDecoration = 'line-through';
+    } else {
+        label.style.textDecoration = 'none';
+    }
+
 }
 
 
@@ -29,12 +38,13 @@ const limparTela = () => {
 
 const atualizandoTela = () => {
     limparTela()
-    Banco.forEach((item, indice) => criandoItem(item.tarefa, item.Status, indice))
+    Banco.forEach((item, indice) => criandoItem(item.tarefa, item.Status, indice));
 }
 const adicionarItemBanco = (evento) => {
     InputTexto = document.querySelector('#input-text');
 
     if (InputTexto.value > " " && evento.type === "click") {
+
         Banco.push({ 'tarefa': InputTexto.value, 'Status': ' ' });
         InputTexto.value = '';
         InputTexto.placeholder = 'Qual é sua tarefa !!';
@@ -44,20 +54,18 @@ const adicionarItemBanco = (evento) => {
 
 
 const ClickCadaItem = (evento) => {
-    
+
     if (evento.target.type === 'button') {
         const indiceElement = evento.target.dataset.indice;
         removeItemBanco(indiceElement)
         atualizandoTela()
-        
-    }else if(evento.target.type === 'checkbox') {
 
+    } else if (evento.target.type === 'checkbox') {
         const checkboxElement = evento.target.dataset.indice;
-    
         marcaCheckbox(checkboxElement);
         atualizandoTela()
     }
-    
+
 }
 
 const removeItemBanco = (indice) => {
@@ -66,25 +74,13 @@ const removeItemBanco = (indice) => {
 }
 
 const marcaCheckbox = (indice) => {
-    Banco[indice].Status = Banco[indice].Status  == ' ' ? 'checked' : ' ';
+    Banco[indice].Status = Banco[indice].Status == ' ' ? 'checked' : ' ';
     atualizandoTela()
 }
 
-const TextStyle=(i)=>{
-     itemBox=document.querySelectorAll('.check')[i];
-
-      label=document.getElementsByClassName('text-list')[i]
-
-     if(itemBox.checked ){
-        label.style.textDecoration='line-through';
-     }else{
-        label.style.textDecoration='none';
-     }
-}
-
+atualizandoTela()
 document.querySelector('#ItemList').addEventListener('click', ClickCadaItem)
 document.querySelector('#btn-enviar').addEventListener('click', adicionarItemBanco)
-atualizandoTela()
 
 
 
